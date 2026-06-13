8cfc31fe30c5c016c3a73d89132541c5d2375e0c
Charalampos Vitoros (Χαράλαμπος Βιτώρος) <charalampos.vitoros@sitecore.com>
2026-06-13 18:03:13 +0300

    wip

--- flappy-bird/src/index.js
+++ flappy-bird/src/index.js
@@ -2,7 +2,7 @@ import Phaser from "phaser";
 
 const config = {
   type: Phaser.AUTO,
-  width: 800,
+  width: 5800,
   height: 600,
   physics: {
     default: "arcade",
@@ -29,12 +29,12 @@ let pipesVerticalDistanceRange = [150, 250];
 let pipeHorizontalDistanceRange = [200, 400];
 
 let pipeHorizontalDistance = 400;
-let PIPES_TO_RENDER = 14;
+let PIPES_TO_RENDER = 3;
 let pipes;
 // ==== end of pipes related stuff
 
 let initialBirdPosition = {
-  x: config.width * 0.1,
+  x: 100,
   y: config.height / 2,
 };
 
@@ -51,7 +51,7 @@ function create() {
   this.add.image(0, 0, "sky").setOrigin(0, 0);
 
   bird = this.physics.add
-    .sprite(config.width * 0.1, config.height / 2, "bird")
+    .sprite(initialBirdPosition.x, initialBirdPosition.y, "bird")
     .setOrigin(0)
     .setGravityY(300);
 
@@ -59,14 +59,14 @@ function create() {
 
   let lastPipeHorizontalDistance = 0;
   for (let i = 0; i < PIPES_TO_RENDER; i++) {
-    let _pipeHorizontalDistance =
-      lastPipeHorizontalDistance +
-      Phaser.Math.Between(...pipeHorizontalDistanceRange);
+    let _pipeHorizontalDistance = getPipeHorizontalDistance(
+      lastPipeHorizontalDistance,
+    );
     lastPipeHorizontalDistance = _pipeHorizontalDistance;
     placePipe.call(this, _pipeHorizontalDistance);
   }
 
-  pipes.setVelocityX(-VELOCITY);
+  // pipes.setVelocityX(-VELOCITY);
 
   // pipes.getChildren().forEach((pipe) => {
   //   pipe.setVelocityY(-VELOCITY);
@@ -75,36 +75,7 @@ function create() {
   this.input.on("pointerdown", flap);
   this.input.keyboard.on("keydown-SPACE", flap);
 }
-
-function flap() {
-  bird.body.setVelocityY(-flopVelocity);
-}
-
-function restartPlayerPosition() {
-  bird.body.setVelocityY(0);
-
-  // bird.body.position = initialBirdPosition;
-  bird.x = initialBirdPosition.x;
-  bird.y = initialBirdPosition.y;
-}
-
-function placePipe(initialDistance) {
-  let _pipeVerticalDistance = Phaser.Math.Between(
-    ...pipesVerticalDistanceRange,
-  );
-  let _pipeVerticalPosition = Phaser.Math.Between(
-    0 + 20,
-    config.height - 20 - _pipeVerticalDistance,
-  );
-
-  const upperPipe = pipes
-    .create(initialDistance, _pipeVerticalPosition, "pipe")
-    .setOrigin(0, 1);
-
-  const lowerPipe = pipes
-    .create(initialDistance, upperPipe.y + _pipeVerticalDistance, "pipe")
-    .setOrigin(0, 0);
-}
+let recycling = false;
 
 // 60fps
 // 60 times per second, 60 * 16.67ms = 1000ms = 1s
@@ -121,10 +92,75 @@ function update(time, delta) {
     restartPlayerPosition();
   }
 
+  if (!recycling) {
+    recyclePipes.call(this);
+  }
+
   // if (bird.body.x >= config.width - bird.width) {
   //   bird.body.setVelocityX(-VELOCITY);
   // }
   // if (bird.body.x <= 0) {
   //   bird.body.setVelocityX(VELOCITY);
   // }
+}
+
+function flap() {
+  bird.body.setVelocityY(-flopVelocity);
+}
+
+function restartPlayerPosition() {
+  bird.body.setVelocityY(0);
+
+  // bird.body.position = initialBirdPosition;
+  bird.x = initialBirdPosition.x;
+  bird.y = initialBirdPosition.y;
+}
+
+function placePipe(initialDistance) {
+  let _pipeVerticalDistance = Phaser.Math.Between(
+    pipesVerticalDistanceRange[0],
+    pipesVerticalDistanceRange[1],
+  );
+  let _pipeVerticalPosition = Phaser.Math.Between(
+    0 + 20,
+    config.height - 20 - _pipeVerticalDistance,
+  );
+
+  const upperPipe = pipes
+    .create(initialDistance, _pipeVerticalPosition, "pipe")
+    .setOrigin(0, 1)
+    .setVelocityX(-VELOCITY);
+
+  const lowerPipe = pipes
+    .create(initialDistance, upperPipe.y + _pipeVerticalDistance, "pipe")
+    .setOrigin(0, 0)
+    .setVelocityX(-VELOCITY);
+}
+
+function recyclePipes() {
+  let firstPipe = pipes.getFirst(true);
+
+  if (firstPipe.x < -firstPipe.width && !recycling) {
+    recycling = true;
+
+    let lastPipe = pipes.getLast(true);
+    let lastPipeDistance = lastPipe.x;
+
+    pipes.killAndHide(firstPipe);
+    firstPipe.setActive(false);
+
+    placePipe.call(this, getPipeHorizontalDistance(lastPipeDistance));
+  } else if (firstPipe.x >= -firstPipe.width) {
+    recycling = false;
+  }
+}
+
+function getPipeHorizontalDistance(lastPipeHorizontalDistance) {
+  let _pipeHorizontalDistance =
+    lastPipeHorizontalDistance +
+    Phaser.Math.Between(
+      pipeHorizontalDistanceRange[0],
+      pipeHorizontalDistanceRange[1],
+    );
+  return _pipeHorizontalDistance;
 }
